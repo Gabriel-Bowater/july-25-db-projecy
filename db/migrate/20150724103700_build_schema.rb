9bf2c47509e7850eb26191e57b91c14dce1eae73
Gabriel-Bowater <gd.bowater@gmail.com>
2015-07-24 11:57:37 +1200

    Build schema

---
+++ db/migrate/20150724103700_build_schema.rb
@@ -0,0 +1,30 @@
+
+
+
+class BuildSchema < ActiveRecord::Migration
+	def change 
+		create_table :animals do |t|
+
+			t.string	:animal_name, :unique, null: false
+			t.string	:animal_ascii_file
+			t.string	:animal_sound, null: false
+			t.string	:animal_sound_file
+
+
+			t.timestamps null: false
+		end
+ 
+		create_table :continents do |t|
+			t.string :continent_name, :unique, null: false
+			t.timestamps null: false
+		end
+
+		create_table :animal_continets do |t|
+			t.belongs_to :continent, index: true
+			t.belongs_to :animal, index: true
+
+			t.timestamps null: false
+		end
+	end
+
+end
