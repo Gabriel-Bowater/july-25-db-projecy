13b21e7f98b83723a5e17989a5e5787d30e73d14
Denis Tyurkov <dtyurkov@gmail.com>
2015-07-24 15:41:07 +1200

    ADD .wav and .txt to DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,32 @@
 require_relative '../config/application.rb'
 
-animal_name = ["dog", 
-				"cat", 
+animal_sound = ["dog.wav",
+				"cat.wav",
+				"bird.wav",
+				"mouse.wav",
+				"cow.wav",
+				"frog.wav",
+				"elephant.wav",
+				"duck.wav",
+				"fish.wav",
+				"seal.wav",
+				"fox.ogg",
+				"horse.wav",
+				"bear.wav",
+				"lion.wav",
+				"rooster.wav",
+				"pig.wav",
+				"whale.wav",
+				"dolphin.wav",
+				"monkey.wav",
+				"skunk.wav",
+				"penguin.wav"]
+
+animal_list = ["dog",
+				"cat",
 				"bird",
 				"mouse",
-				"cow", 
+				"cow",
 				"frog",
 				"elephant",
 				"duck",
@@ -22,48 +44,72 @@ animal_name = ["dog",
 				"skunk",
 				"penguin"]
 
+animal_art = ["dog.txt",
+				"cat.txt",
+				"bird.txt",
+				"mouse.txt",
+				"cow.txt",
+				"frog.txt",
+				"elephant.txt",
+				"duck.txt",
+				"fish.txt",
+				"seal.txt",
+				"fox.ogg",
+				"horse.txt",
+				"bear.txt",
+				"lion.txt",
+				"rooster.txt",
+				"pig.txt",
+				"whale.txt",
+				"dolphin.txt",
+				"monkey.txt",
+				"skunk.txt",
+				"penguin.txt"]
 
-animal_sound = ["woof",
-				"meow",
-				"tweet",
-				"squeek",
-				"moo",
-				"croak",
-				"toot",
-				"quack",
-				"blub",
-				"ow-ow-ow-ow",
-				"ding ling ding ring",
-				"ah-ooh-ga",
-				"growl",
-				"roar",
-				"cock-a-doodle-doo",
-				"oink",
-				"mwoooo wwwweee",
-				"eeek eek eek",
-				"whoo whoo whoo",
-				"fart",
-				"meep meep"]
+# animal_sound = ["woof",
+# 				"meow",
+# 				"tweet",
+# 				"squeek",
+# 				"moo",
+# 				"croak",
+# 				"toot",
+# 				"quack",
+# 				"blub",
+# 				"ow-ow-ow-ow",
+# 				"ding ling ding ring",
+# 				"ah-ooh-ga",
+# 				"growl",
+# 				"roar",
+# 				"cock-a-doodle-doo",
+# 				"oink",
+# 				"mwoooo wwwweee",
+# 				"eeek eek eek",
+# 				"whoo whoo whoo",
+# 				"fart",
+# 				"meep meep"]
 
 
-continent_name = ["North America",
-					"Asia",
-					"Africa",
-					"South America",
-					"Antarctica",
-					"Europe",
-					"Australia"]
+# continent_name = ["North America",
+# 					"Asia",
+# 					"Africa",
+# 					"South America",
+# 					"Antarctica",
+# 					"Europe",
+# 					"Australia"]
 
 
-20.times do |i|
-	Animal.create(
-		:animal_name => animal_name[i],
-		:animal_sound => animal_sound[i]
-		)
+	animals = Animal.all
+animals.length.times do |i|
+	p animals[i]
+	animals[i].update(
+	:animal_ascii_file => animal_art[i],
+	:animal_sound_file => animal_sound[i])
+	p animals[i]
+	animals.save
 end
 
-7.times do |x|
-	Continent.create(
-		:continent_name => continent_name[x]
-		)
-end
+# 7.times do |x|
+# 	Continent.create(
+# 		:continent_name => continent_name[x]
+# 		)
+# end
